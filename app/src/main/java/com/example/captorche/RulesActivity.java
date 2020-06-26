93fdcf7896c2c5d343cac09437dc4abd97a5186a
ydauvin <51073361+ydauvin@users.noreply.github.com>
2020-06-26 17:56:18 +0200

    changement textView

--- app/src/main/java/com/example/captorche/RulesActivity.java
+++ app/src/main/java/com/example/captorche/RulesActivity.java
@@ -3,13 +3,12 @@ package com.example.captorche;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RulesActivity extends AppCompatActivity {
-    private Button button;
+    private View next;
     private TextView textView;
 
     @Override
@@ -17,9 +16,9 @@ public class RulesActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
-        button = (Button) findViewById(R.id.button);
+        next = (View) findViewById(R.id.next);
         final TextView rules = (TextView) findViewById (R.id.rules);
-        button.setOnClickListener(new View.OnClickListener() {
+        next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 rules.setText("ssgdsg");
             }
