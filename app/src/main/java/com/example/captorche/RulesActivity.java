6ace9eb19cf565fb6d45d29133782ea147ae0dbf
ydauvin <51073361+ydauvin@users.noreply.github.com>
2020-07-04 15:52:14 +0200

    choix d'une regle aleatoire dans la liste essai de recuperation des regle en ligne (ca marche pas)

--- app/src/main/java/com/example/captorche/RulesActivity.java
+++ app/src/main/java/com/example/captorche/RulesActivity.java
@@ -1,26 +1,47 @@
 package com.example.captorche;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
+import java.net.*;
+import java.io.*;
+import static java.util.Random.*;
+
 
 public class RulesActivity extends AppCompatActivity {
     private View next;
     private TextView textView;
-
+    private String[] consigne = {"C'est l'heure de l'apéro quelque part dans le monde, donc tout le monde prend 2","yec'hed mat","Si tu as codé l'appli prend un shot"};
+    private int rnd;
+    private String web="";
+    //http://ydauvin.free.fr/CapTorche/regle/regle.json
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try{
+            URL url = new URL("http://ydauvin.free.fr/CapTorche/regle/regle.txt");
+            InputStreamReader isr  = new InputStreamReader(url.openStream());
+            BufferedReader in = new BufferedReader(isr);
+            String inputLine;
+            while ((inputLine = in.readLine()) != null){
+                web+=inputLine;
+            }
+            in.close();
+            System.out.println(web);
 
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
         next = findViewById(R.id.next);
         final TextView rules = findViewById (R.id.rules);
         next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                rules.setText("ssgdsg");
+                    rnd = new Random().nextInt(consigne.length);
+                    rules.setText(consigne[rnd]);
+
             }
 
         });
